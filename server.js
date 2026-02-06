fe7df6c681a2c97a23a7eb272d5d293652ee947b
NgDanhThanhTrung <thanhtrungnguyendanh@gmail.com>
2026-02-06 16:09:57 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`fx580 Live: http://localhost:${PORT}`));
